cdac5d8cdbd7c2cd01d68bed212c0e69b83c1064
Vitor Xavier <vitorvxs@live.com>
2020-02-27 17:55:27 -0300

    Add Parellel e GetData

---
+++ src/Api/Dto/CaracteristicaParametros.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Dto
+{
+    public class CaracteristicaParametros
+    {
+        public string TabelaCaracteristica { get; set; }
+        public string DescricaoCaracteristica { get; set; }
+        public string ValorFatorCaracteristica { get; set; }
+        public string ExercicioCaracteristica { get; set; }
+    }
+}
--- src/Implementation/GenericValueLanguage.cs
+++ src/Implementation/GenericValueLanguage.cs
@@ -1,78 +1,54 @@
-﻿
+﻿using System;
 
-using System;
-
-public class GenericValueLanguage
+    public class GenericValueLanguage
     {
-        public readonly static GenericValueLanguage VOID = new GenericValueLanguage(new Object());
+        public readonly static GenericValueLanguage VOID = new GenericValueLanguage(new object());
 
-        public readonly Object Value;
+        public readonly object Value;
         public readonly bool Contant;
 
-        public GenericValueLanguage(Object value, bool constat = false)
+        public GenericValueLanguage(object value, bool constat = false)
         {
             Value = value;
             Contant = constat;
         }
 
-        public Boolean AsBoolean()
-        {
-            return Boolean.Parse(Value.ToString());
-        }
+        public bool AsBoolean() => bool.Parse(Value.ToString());
 
-        public Double AsDouble()
-        {
-            return Double.Parse(Value.ToString());
-        }
+        public double AsDouble() => double.Parse(Value.ToString());
 
-        public DateTime AsDateTime()
-        {
-            return DateTime.Parse(Value.ToString());
-        }
+        public DateTime AsDateTime() =>  DateTime.Parse(Value.ToString());
 
         public static explicit operator double(GenericValueLanguage GenericValueLanguage)
         {
             if (GenericValueLanguage == null) return 0;
-            double decimalConvert;
-            double.TryParse(GenericValueLanguage.Value.ToString(), out decimalConvert);
+            double.TryParse(GenericValueLanguage.Value.ToString(), out double decimalConvert);
             return decimalConvert;
 
         }
         public static explicit operator int(GenericValueLanguage GenericValueLanguage)
         {
-            int intConvert;
-            int.TryParse(GenericValueLanguage.Value.ToString(), out intConvert);
+            int.TryParse(GenericValueLanguage.Value.ToString(), out int intConvert);
             return intConvert;
         }
 
-        public String AsString()
-        {
-            return Value.ToString();
-        }
+        public string AsString() => Value.ToString();
 
-        public bool IsDouble()
-        {
-            return Value is Double;
-        }
+        public bool IsDouble() => Value is double;
 
-        public bool IsDate()
-        {
-            return Value is DateTime;
-        }
+        public bool IsDate() => Value is DateTime;
 
-        public override int GetHashCode()
-        {
-            return Value == null ? 0 : Value.GetHashCode();
-        }
+        public override int GetHashCode() =>
+            Value == null ? 0 : Value.GetHashCode();
 
-        public override bool Equals(Object o)
+        public override bool Equals(object o)
         {
             if (Value == o)
             {
                 return true;
             }
 
-            if (Value == null || o == null)
+            if (Value is null || o is null)
             {
                 return false;
             }
@@ -82,14 +58,7 @@ public class GenericValueLanguage
             return Value.Equals(that.Value);
         }
 
-        public override String ToString()
-        {
-            if (IsDouble())
-            {
-                return Value.ToString().Replace(',', '.');
-            }
-
-            return Value.ToString();
-        }
+        public override string ToString() =>
+            IsDouble() ? Value.ToString().Replace(',', '.') : Value.ToString();
     }
 
