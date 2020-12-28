c7aa7a006491d7c89b4538e892627fedefcf9cbf
Vitor Xavier de Souza <vitorvxs@live.com>
2020-12-28 12:28:40 -0300

    Alterações Common

---
+++ src/Common/Enums/SetorOrigem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Common.Enums
+{
+    public enum SetorOrigem
+    {
+        [Description("Imobiliário")]
+        Imobiliario,
+
+        [Description("Contribuinte")]
+        Contribuinte,
+
+        [Description("Global")]
+        Global
+    }
+}
---
+++ src/Common/Helpers/SetorOrigemHelper.cs
@@ -0,0 +1,41 @@
+﻿using Common.Enums;
+using Common.Exceptions;
+using System;
+
+namespace Common.Helpers
+{
+    public static class SetorOrigemHelper
+    {
+        public static string[] GetTabelasRelacionadas(SetorOrigem setor) => setor switch
+        {
+            SetorOrigem.Imobiliario => new string[] { "FacesQuadra" },
+            _ => Array.Empty<string>()
+        };
+
+        /// <summary>
+        /// Busca a tabela principal referente ao Setor Origem informado.
+        /// </summary>
+        /// <param name="setor">Setor Origem</param>
+        /// <returns>Nome da tabela principal</returns>
+        public static string GetTabelaPrincipal(SetorOrigem setor) => setor switch
+        {
+            SetorOrigem.Imobiliario => "Fisicos",
+            SetorOrigem.Contribuinte => "Contribuintes",
+            SetorOrigem.Global => "Global",
+            _ => throw new BadRequestException("Setor não possui uma tabela principal configurada.")
+        };
+
+        /// <summary>
+        /// Busca as tabelas referentes ao Setor Origem informado.
+        /// </summary>
+        /// <param name="setor"></param>
+        /// <returns></returns>
+        public static string[] GetTabelasSetor(SetorOrigem setor) => setor switch
+        {
+            SetorOrigem.Imobiliario => new string[] { "Fisicos", "FisicoOutros", "FisicoAreas", "FacesQuadra" },
+            SetorOrigem.Contribuinte => new string[] { "Contribuintes" },
+            SetorOrigem.Global => new string[] { "Caracteristica", "Parametro" },
+            _ => throw new BadRequestException("Setor não possui tabelas configuradas.")
+        };
+    }
+}
